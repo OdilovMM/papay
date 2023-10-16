29a51e5fa71b00fadb91ac1c3a0191035280fe89
OdilovMM <dssrinman@gmail.com>
2023-10-16 23:35:38 +0900

    feat: member login process development

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -10,16 +10,35 @@ memberController.signup = async (req, res) => {
     const member = new Member();
     const new_member = await member.signupData(data);
 
-    res.json({state: 'succeed', datsa: new_member});
+    res.json({state: 'succeed', data: new_member});
   } catch (error) {
     console.log(`ERROR, cont/signup, ${error.message}`);
     res.json({state: 'fail', message: error.message})
   }
 };
-memberController.login = (req, res) => {
-  console.log("POST cont/login");
-  res.send("login sahifasi");
+
+
+
+
+memberController.login = async (req, res) => {
+  try {
+    console.log("POST: cont/login");
+    const data = req.body;
+    console.log("body:::", req.body);
+    const member = new Member();
+    const result = await member.loginData(data);
+
+
+    res.json({state: 'succeed', data: result});
+  } catch (error) {
+    console.log(`ERROR, cont/login, ${error.message}`);
+    res.json({state: 'fail', message: error.message})
+  }
 };
+
+
+
+
 memberController.logout = (req, res) => {
   console.log("POST cont/logout");
   res.send("logout sahifasi");
--- models/Member.js
+++ models/Member.js
@@ -1,10 +1,12 @@
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
+const assert = require("assert");
 class Member {
   constructor() {
     this.memberModel = MemberModel;
   }
 
+  //signup
   async signupData(input) {
     try {
       const new_member = new this.memberModel(input);
@@ -21,6 +23,29 @@ class Member {
       result.mb_password = "";
 
       return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //login
+  async loginData(input) {
+    try {
+      const member = await this.memberModel
+        .findOne(
+          { mb_nick: input.mb_nick },
+          { mb_nick: 1, mb_password: 1, _id: 0 }
+        )
+        .exec();
+
+      assert.ok(member, Definer.auth_err3);
+      // console.log(member);
+
+      const isMatch = input.mb_password === member.mb_password;
+      assert.ok(member, Definer.auth_err4);
+      return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
+
+      console.log("member::::", member);
     } catch (error) {
       throw error;
     }
